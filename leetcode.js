951469ed51440f0f31f02d79c320a8011f9838fb
Priya <priyakshetty26@gmail.com>
2025-03-30 13:09:30 +0530

    leetcode

--- leetcode.js
+++ leetcode.js
@@ -12,3 +12,26 @@ let t = Date.now();
 sleep(100)
   .then(() => console.log(Date.now() - t))
   .catch((error) => console.log(error)); // 100
+
+//Array and Object
+var isEmpty = function (obj) {
+  //check if obj or array
+  //then check length
+
+  const array = Array.isArray(obj);
+  const object = Object.prototype.toString.call(obj) === "[object Object]";
+
+  console.log(array, object);
+
+  if (array) {
+    return obj.length === 0 ? true : false;
+  } else if (object) {
+    return Object.keys(obj).length === 0 ? true : false;
+  }
+};
+
+// console.log(isEmpty({}));
+
+
+//Palindrome negative number
+
